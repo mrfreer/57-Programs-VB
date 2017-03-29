316db760efe15831327912016321b611168cbb2c
dfreer <dfreer@6163-343591>
2017-03-29 16:16:32 -0400

    Gallons calculator.

---
+++ 57 Programs VB/Program9.Designer.vb
@@ -0,0 +1,108 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Program9
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.LabelResult = New System.Windows.Forms.Label()
+        Me.MaskedTextBox1 = New System.Windows.Forms.MaskedTextBox()
+        Me.MaskedTextBox2 = New System.Windows.Forms.MaskedTextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(47, 30)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(43, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Length:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(50, 67)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(38, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Width:"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(50, 146)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(175, 23)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Calculate"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'LabelResult
+        '
+        Me.LabelResult.AutoSize = True
+        Me.LabelResult.Location = New System.Drawing.Point(50, 112)
+        Me.LabelResult.Name = "LabelResult"
+        Me.LabelResult.Size = New System.Drawing.Size(0, 13)
+        Me.LabelResult.TabIndex = 5
+        '
+        'MaskedTextBox1
+        '
+        Me.MaskedTextBox1.Location = New System.Drawing.Point(97, 30)
+        Me.MaskedTextBox1.Mask = "00000"
+        Me.MaskedTextBox1.Name = "MaskedTextBox1"
+        Me.MaskedTextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.MaskedTextBox1.TabIndex = 0
+        '
+        'MaskedTextBox2
+        '
+        Me.MaskedTextBox2.Location = New System.Drawing.Point(97, 67)
+        Me.MaskedTextBox2.Mask = "0000"
+        Me.MaskedTextBox2.Name = "MaskedTextBox2"
+        Me.MaskedTextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.MaskedTextBox2.TabIndex = 1
+        '
+        'Program9
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(354, 403)
+        Me.Controls.Add(Me.MaskedTextBox2)
+        Me.Controls.Add(Me.MaskedTextBox1)
+        Me.Controls.Add(Me.LabelResult)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Program9"
+        Me.Text = "Program9"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents LabelResult As Label
+    Friend WithEvents MaskedTextBox1 As MaskedTextBox
+    Friend WithEvents MaskedTextBox2 As MaskedTextBox
+End Class
---
+++ 57 Programs VB/Program9.vb
@@ -0,0 +1,7 @@
+﻿Public Class Program9
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim area As Double = MaskedTextBox1.Text * MaskedTextBox2.Text
+        Dim gallons As Integer = Math.Ceiling(area / 350)
+        LabelResult.Text = "The area is " & area & " and " & gallons & " gallons are needed."
+    End Sub
+End Class
