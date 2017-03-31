2c603979f82e6d408bbad06965d2a77d019bd9f4
dfreer <dfreer@6163-343591>
2017-03-31 15:29:15 -0400

    Adding textboxes programmatically and event handlers programmatically.

---
+++ 57 Programs VB/Program10.Designer.vb
@@ -0,0 +1,63 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Program10
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.Button1 = New System.Windows.Forms.Button()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Location = New System.Drawing.Point(249, 37)
+        Me.NumericUpDown1.Maximum = New Decimal(New Integer() {5, 0, 0, 0})
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(171, 20)
+        Me.NumericUpDown1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(249, 80)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(171, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Select Number of Items"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Program10
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(784, 605)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Name = "Program10"
+        Me.Text = "Program10"
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents NumericUpDown1 As NumericUpDown
+    Friend WithEvents Button1 As Button
+End Class
---
+++ 57 Programs VB/Program10.vb
@@ -0,0 +1,49 @@
+﻿Public Class Program10
+    Dim numTextBoxes As Integer
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        numTextBoxes = NumericUpDown1.Value
+        Dim allTextBoxes(numTextBoxes) As TextBox
+        For a As Integer = 0 To numTextBoxes - 1
+            Dim tb As New TextBox
+            Dim lbl As New Label
+
+            tb.Name = "TextBox" + a.ToString
+            lbl.Name = "Label" + a.ToString
+            lbl.Text = "Enter Item:"
+            tb.Location = New Point(200, 200 + (a * 25))
+            lbl.Location = New Point(100, 200 + (a * 25))
+            Me.Controls.Add(tb)
+            Me.Controls.Add(lbl)
+            allTextBoxes(a) = tb
+        Next
+
+        Dim btn As New Button
+        btn.Location = New Point(200, 200 + (numTextBoxes * 25))
+        btn.Text = "Add Orders"
+        btn.Name = "AddInfo"
+        AddHandler btn.Click, AddressOf addInfo
+        Me.Controls.Add(btn)
+
+    End Sub
+    Dim totalCost As Double = 0
+    Sub addInfo(sender As Object, e As EventArgs)
+        totalCost = 0
+        Dim value As String = ""
+        For Each txtBox As TextBox In Me.Controls.OfType(Of TextBox)()
+            value = txtBox.Text
+
+            If value = "Socks" Then
+                totalCost += 4.5
+            ElseIf value = "Shirt" Then
+                totalCost += 10
+            ElseIf value = "Pants" Then
+                totalCost += 15
+            Else
+                totalCost += 7
+            End If
+        Next
+
+        MsgBox("$" + totalCost.ToString("F2") + " is the total cost for " + value + ".")
+    End Sub
+
+End Class
