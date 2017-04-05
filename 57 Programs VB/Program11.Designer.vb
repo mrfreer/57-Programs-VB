719171ea879f88317333fd572dd5bfb7c031bf89
dfreer <dfreer@6163-343591>
2017-04-05 15:08:45 -0400

    Completed Program11 to convert currency.

---
+++ 57 Programs VB/Program11.Designer.vb
@@ -0,0 +1,109 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Program11
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.LabelResult = New System.Windows.Forms.Label()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.MaskedTextBox1 = New System.Windows.Forms.MaskedTextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(125, 373)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(175, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Calculate Exchange"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'LabelResult
+        '
+        Me.LabelResult.Location = New System.Drawing.Point(12, 18)
+        Me.LabelResult.Name = "LabelResult"
+        Me.LabelResult.Size = New System.Drawing.Size(188, 25)
+        Me.LabelResult.TabIndex = 1
+        Me.LabelResult.Text = "Enter an amount of US Dollars $"
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(114, 122)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(186, 200)
+        Me.RichTextBox1.TabIndex = 2
+        Me.RichTextBox1.Text = ""
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"AUD", "BGN", "BRL", "CAD", "CHF", "CNY", "CZK", "DKK", "GBP", "HKD", "HRK", "HUF", "IDR", "ILS", "INR", "JPY", "KRW", "MXN", "MYR", "NOK", "NZD", "PHP", "PLN", "RON", "RUB", "SEK", "SGD", "THB", "TRY", "ZAR", "EUR"})
+        Me.ComboBox1.Location = New System.Drawing.Point(114, 59)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(186, 21)
+        Me.ComboBox1.TabIndex = 3
+        '
+        'MaskedTextBox1
+        '
+        Me.MaskedTextBox1.Location = New System.Drawing.Point(195, 18)
+        Me.MaskedTextBox1.Mask = "000000000"
+        Me.MaskedTextBox1.Name = "MaskedTextBox1"
+        Me.MaskedTextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.MaskedTextBox1.TabIndex = 4
+        Me.MaskedTextBox1.ValidatingType = GetType(Integer)
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(15, 59)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(93, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Select a currency:"
+        '
+        'Program11
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(421, 433)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.MaskedTextBox1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.LabelResult)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Program11"
+        Me.Text = "Program11"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents LabelResult As Label
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents MaskedTextBox1 As MaskedTextBox
+    Friend WithEvents Label1 As Label
+End Class
---
+++ 57 Programs VB/Program11.vb
@@ -0,0 +1,19 @@
+﻿Imports Newtonsoft.Json
+
+Public Class Program11
+
+    Dim result As String
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim webClient As New System.Net.WebClient
+        result = webClient.DownloadString("http://api.fixer.io/latest?base=USD")
+
+        Dim obj = JsonConvert.DeserializeObject(result)
+        'RichTextBox1.Text = obj("rates").ToString
+        Dim newAmount As Double = MaskedTextBox1.Text * CDbl(obj("rates")(ComboBox1.Text))
+        RichTextBox1.Text = "You entered $" & MaskedTextBox1.Text & vbCrLf &
+            "The exchange rate is currently " & obj("rates")(ComboBox1.Text) _
+            & vbCrLf & "The value will be " & newAmount.ToString
+        'I wanted to use a class to deserialize the object 
+        'but the anonymous access is convenient 
+    End Sub
+End Class
