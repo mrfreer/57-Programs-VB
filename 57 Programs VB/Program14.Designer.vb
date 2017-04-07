c81df3a86f5fa0bd1d19077f9b2c6c5d16835277
dfreer <dfreer@6163-343591>
2017-04-07 16:25:18 -0400

    Random adding sales tax and determining the final price.

---
+++ 57 Programs VB/Program14.Designer.vb
@@ -0,0 +1,99 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Program14
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.MaskedTextBox1 = New System.Windows.Forms.MaskedTextBox()
+        Me.LabelResult = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(328, 187)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"})
+        Me.ComboBox1.Location = New System.Drawing.Point(12, 256)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(328, 21)
+        Me.ComboBox1.TabIndex = 2
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 284)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(328, 23)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Calculate Taxes"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'MaskedTextBox1
+        '
+        Me.MaskedTextBox1.Location = New System.Drawing.Point(13, 230)
+        Me.MaskedTextBox1.Mask = "0000000000"
+        Me.MaskedTextBox1.Name = "MaskedTextBox1"
+        Me.MaskedTextBox1.Size = New System.Drawing.Size(327, 20)
+        Me.MaskedTextBox1.TabIndex = 1
+        '
+        'LabelResult
+        '
+        Me.LabelResult.AutoSize = True
+        Me.LabelResult.Location = New System.Drawing.Point(12, 314)
+        Me.LabelResult.Name = "LabelResult"
+        Me.LabelResult.Size = New System.Drawing.Size(13, 13)
+        Me.LabelResult.TabIndex = 4
+        Me.LabelResult.Text = "$"
+        '
+        'Program14
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(352, 492)
+        Me.Controls.Add(Me.LabelResult)
+        Me.Controls.Add(Me.MaskedTextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Program14"
+        Me.Text = "Program14"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents MaskedTextBox1 As MaskedTextBox
+    Friend WithEvents LabelResult As Label
+End Class
---
+++ 57 Programs VB/Program14.vb
@@ -0,0 +1,15 @@
+﻿Public Class Program14
+    Dim list As New List(Of Double)
+    Private Sub Program14_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim r As New Random(5)
+        For i As Integer = 0 To 49
+            list.Add(r.NextDouble() * 15)
+        Next
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim total As Double = (MaskedTextBox1.Text) * (1 + list.Item(ComboBox1.SelectedIndex) / 100)
+        LabelResult.Text = "Total cost $" & total.ToString("N2") & " based on taxes of " &
+            list.Item(ComboBox1.SelectedIndex).ToString("N2") & " %"
+    End Sub
+End Class
