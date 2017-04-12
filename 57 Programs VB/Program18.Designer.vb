573341ed035225cea70a3fa25aef685b79439bde
dfreer <dfreer@6163-343591>
2017-04-12 11:56:26 -0400

    Celsius to Fahrenheit conversion.

---
+++ 57 Programs VB/Program18.Designer.vb
@@ -0,0 +1,89 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Program18
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TrackBar1 = New System.Windows.Forms.TrackBar()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.LabelConverted = New System.Windows.Forms.Label()
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TrackBar1
+        '
+        Me.TrackBar1.Location = New System.Drawing.Point(62, 145)
+        Me.TrackBar1.Maximum = 150
+        Me.TrackBar1.Minimum = -50
+        Me.TrackBar1.Name = "TrackBar1"
+        Me.TrackBar1.Size = New System.Drawing.Size(390, 45)
+        Me.TrackBar1.TabIndex = 0
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Celsius", "Fahrenheit"})
+        Me.ComboBox1.Location = New System.Drawing.Point(62, 114)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(390, 21)
+        Me.ComboBox1.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(62, 95)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(122, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Your Temperature Units:"
+        '
+        'LabelConverted
+        '
+        Me.LabelConverted.AutoSize = True
+        Me.LabelConverted.Location = New System.Drawing.Point(62, 208)
+        Me.LabelConverted.Name = "LabelConverted"
+        Me.LabelConverted.Size = New System.Drawing.Size(59, 13)
+        Me.LabelConverted.TabIndex = 3
+        Me.LabelConverted.Text = "Converted:"
+        '
+        'Program18
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(489, 457)
+        Me.Controls.Add(Me.LabelConverted)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.TrackBar1)
+        Me.Name = "Program18"
+        Me.Text = "Program18"
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TrackBar1 As TrackBar
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents LabelConverted As Label
+End Class
---
+++ 57 Programs VB/Program18.vb
@@ -0,0 +1,19 @@
+﻿Public Class Program18
+    Private Sub TrackBar1_Scroll(sender As Object, e As EventArgs) Handles TrackBar1.Scroll
+        Dim converted As Double = 0.0
+        Dim result As String = ""
+        If ComboBox1.Text = "Celsius" Then
+            'convert to f
+            converted = (TrackBar1.Value * (9 / 5.0)) + 32
+            result = "Celsius value " + TrackBar1.Value.ToString("N2") + " The temperature in Fahrenheit is " + converted.ToString("N2")
+        Else
+            'convert to c.
+            converted = (TrackBar1.Value - 32) * (5 / 9.0)
+            result = "Fahrenheit value " + TrackBar1.Value.ToString("N2") + " The temperature in Celsius is " + converted.ToString("N2")
+        End If
+        LabelConverted.Text = result
+
+    End Sub
+
+
+End Class
